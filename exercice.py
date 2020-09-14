e926ddf1ffb567da9c2c1e231742682de6d4b548
JolanLec <jolan.le-clainche@polymtl.ca>
2020-09-14 22:49:11 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,7 @@ def square(a: float) -> float:
 def average(a: float, b: float, c: float) -> float:
     return 0.0
 
-)c+b+a
+3/()c+b+a
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     return 0.0
 
