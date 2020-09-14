7099cf0dead63fa11bc7829e7f4f05347c1f63e9
JolanLec <jolan.le-clainche@polymtl.ca>
2020-09-14 23:39:42 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,7 +20,7 @@ def to_degrees(angle_rads: float) -> tuple:
     degrees:math.degrees(angle_rads)
     minutes: (degrees % 1)*60
     secondes: (minutes % 1)*60
-    return 0.0, 0.0, 0.0
+    return degrees, minutes, 0.0
 
 
 def to_celsius(temperature: float) -> float:
