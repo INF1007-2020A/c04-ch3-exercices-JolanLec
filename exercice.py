74cffd74642f84721f3e6a6bf1f65e32cf6a666a
JolanLec <jolan.le-clainche@polymtl.ca>
2020-09-14 22:54:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def average(a: float, b: float, c: float) -> float:
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     return 0.0
 
-  × π/180*sged_elgna
+ip  × π/180*sged_elgna
 def to_degrees(angle_rads: float) -> tuple:
     return 0.0, 0.0, 0.0
 
